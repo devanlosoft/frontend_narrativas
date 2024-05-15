7725691fe3a1d522a2a9dc1c97c375a69dce456a
devanlosoft <alopezleon95@correo.unicordoba.edu.co>
2024-05-15 10:26:41 -0500

    carrousel updated

--- app/ui/compontents/carousel.tsx
+++ app/ui/compontents/carousel.tsx
@@ -5,27 +5,27 @@ import 'slick-carousel/slick/slick-theme.css';
 import { v4 as uuidv4 } from 'uuid';
 import Card from './card';
 
-export default function Carousel () {
+export default function Carousel() {
   const carouselItems = [
     {
       image:
         'https://img.freepik.com/foto-gratis/papel-pintado-gatito-lindo-fantasia_1409-6188.jpg',
       title: 'Tiempo, más allá del reloj',
       description: 'descripcion para imagen 1',
-      button_url: 'facebook.com'
+      button_url: 'facebook.com',
     },
     {
       image: 'https://i.blogs.es/0ca9a6/aa/1366_2000.jpeg',
-      title: 'Formas de relacionamiento con el rio',
+      title: 'Formas de relacionamiento con el rio parte 2',
       description: 'Una vision mas alla del barrio',
-      button_url: 'x.com'
+      button_url: 'x.com',
     },
     {
       image:
         'https://media.es.wired.com/photos/6501e7429fa9000811a95fe8/16:9/w_2240,c_limit/Adobe%20Firefly.jpeg',
       title: 'Titulo para imagen 3',
       description: 'descripcion para imagen 3',
-      button_url: 'instagram.com'
+      button_url: 'instagram.com',
     },
   ];
 
@@ -56,4 +56,4 @@ export default function Carousel () {
       </div>
     </div>
   );
-};
+}
